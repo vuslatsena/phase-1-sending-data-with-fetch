c8404a4a7276425ffbdeb78772020a420e808734
vuslatsena <vuslatsena23@gmail.com>
2023-04-29 08:31:26 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData(name, email) {
+  const formData = { name: name, email: email };
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+  
+  return fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(function(data){const id = data.id;
+          document.body.append(id);})
+    .catch(function(error) {
+      document.body.append(error.message);
+    });
+}
